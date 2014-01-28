75a9e276ccef0608730bfb469377e3894d96720c
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-01-28 23:16:49 +0000

    CLEANUP
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/simplestspam/trunk@5442 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/form/SimplestSpamProtector.php
+++ code/form/SimplestSpamProtector.php
@@ -23,23 +23,22 @@ class SimplestSpamProtector implements SpamProtector {
 	 * @return bool
 	 */
 	function updateForm($form, $before=null, $fieldsToSpamServiceMapping=null) {
-
 		$this->field = $this->getFormField("SimplestSpamField", "Please answer this question to prove you are a real human", null, $form);
-
 		if ($before && $form->Fields()->fieldByName($before)) {
 			$form->Fields()->insertBefore($this->field, $before);
 		}
 		else {
 			$form->Fields()->push($this->field);
 		}
-
 		return $form->Fields();
 	}
 
+
+	/*
 	function setFieldMapping($fieldToPostTitle, $fieldsToPostBody=null, $fieldToAuthorName=null, $fieldToAuthorUrl=null, $fieldToAuthorEmail=null, $fieldToAuthorOpenId=null) {
 
 	}
-
+	*/
 
 	public function getFormField($name = null, $title = null, $value = null, $form = null, $rightTitle = null) {
 		return new SimplestSpamField($name, $title, $value, $form, $rightTitle);
