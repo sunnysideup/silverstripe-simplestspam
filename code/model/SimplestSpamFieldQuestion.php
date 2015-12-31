ffdffa45581c5fa58f5ae79bca1ca3ece5b2192a
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:20:43 +1300

    Converted to PSR-2

--- code/model/SimplestSpamFieldQuestion.php
+++ code/model/SimplestSpamFieldQuestion.php
@@ -1,30 +1,31 @@
 <?php
 
-class SimplestSpamFieldQuestion extends DataObject {
-	//database
-	private static $db = array(
-		"Question" => "Varchar(100)",
-		"Answer" => "Varchar(50)"
-	);
-	//formatting
-	private static $searchable_fields = array("Question" => "PartialMatchFilter");
+class SimplestSpamFieldQuestion extends DataObject
+{
+    //database
+    private static $db = array(
+        "Question" => "Varchar(100)",
+        "Answer" => "Varchar(50)"
+    );
+    //formatting
+    private static $searchable_fields = array("Question" => "PartialMatchFilter");
 
-	private static $summary_fields = array("Question" => "Question", "Answer" => "Answer");
+    private static $summary_fields = array("Question" => "Question", "Answer" => "Answer");
 
-	private static $singular_name = "Spam filter question";
+    private static $singular_name = "Spam filter question";
 
-	private static $plural_name = "Spam filter questions";
+    private static $plural_name = "Spam filter questions";
 
-	function requireDefaultRecords() {
-		parent::requireDefaultRecords();
-		$simplestSpamFieldQuestion = SimplestSpamFieldQuestion::get()->First();
-		if(!$simplestSpamFieldQuestion) {
-			$simplestSpamFieldQuestion = new SimplestSpamFieldQuestion();
-			$simplestSpamFieldQuestion->Question = 'What is the original name for New Zealand?';
-			$simplestSpamFieldQuestion->Answer = 'Aotearoa';
-			$simplestSpamFieldQuestion->write();
-			DB::alteration_message($simplestSpamFieldQuestion->ClassName."Created default entry for SimplestSpamFieldQuestion", 'created');
-		}
-	}
-
+    public function requireDefaultRecords()
+    {
+        parent::requireDefaultRecords();
+        $simplestSpamFieldQuestion = SimplestSpamFieldQuestion::get()->First();
+        if (!$simplestSpamFieldQuestion) {
+            $simplestSpamFieldQuestion = new SimplestSpamFieldQuestion();
+            $simplestSpamFieldQuestion->Question = 'What is the original name for New Zealand?';
+            $simplestSpamFieldQuestion->Answer = 'Aotearoa';
+            $simplestSpamFieldQuestion->write();
+            DB::alteration_message($simplestSpamFieldQuestion->ClassName."Created default entry for SimplestSpamFieldQuestion", 'created');
+        }
+    }
 }
