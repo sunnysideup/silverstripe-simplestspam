d184f9b51965c29b62d98bbab45febf6d3108927
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2018-05-22 17:13:19 +1200

    PATCH: PSR-2

--- code/form/SimplestSpamField.php
+++ code/form/SimplestSpamField.php
@@ -5,58 +5,61 @@
  *
  * @module SimplestSpam
  */
-class SimplestSpamField extends SpamProtectorField {
+class SimplestSpamField extends SpamProtectorField
+{
+    private static $questions_and_answers = array();
 
-	private static $questions_and_answers = array();
+    private static $has_been_initialised = false;
 
-	private static $has_been_initialised = false;
+    protected $error = '';
 
-	protected $error = '';
+    protected function initialise()
+    {
+        if (!self::$has_been_initialised) {
+            if (!count(self::$questions_and_answers)) {
+                self::$questions_and_answers = SimplestSpamFieldQuestion::get();
+            }
+            $questionCount = 0;
+            if (self::$questions_and_answers->count()) {
+                $questionCount = self::$questions_and_answers->count();
+            }
+            if (!isset($_REQUEST['SimplestSpam_challenge_field']) && $questionCount) {
+                $randomNumber = rand(0, $questionCount - 1);
+                Session::set("SimplestSpamQuestion", $randomNumber + 1); // adding one to make it easier to work out if anything has been entered, i.e. 0 could be nothing or first question
+            }
+        }
+        self::$has_been_initialised = true;
+    }
 
-	protected function initialise() {
-		if(!self::$has_been_initialised) {
-			if(!count(self::$questions_and_answers)) {
-				self::$questions_and_answers = SimplestSpamFieldQuestion::get();
-			}
-			$questionCount = 0;
-			if(self::$questions_and_answers->count()) {
-				$questionCount = self::$questions_and_answers->count();
-			}
-			if(!isset($_REQUEST['SimplestSpam_challenge_field']) && $questionCount) {
-				$randomNumber = rand(0, $questionCount - 1);
-				Session::set("SimplestSpamQuestion", $randomNumber + 1); // adding one to make it easier to work out if anything has been entered, i.e. 0 could be nothing or first question
-			}
-		}
-		self::$has_been_initialised = true;
-	}
+    public function Field($properties = array())
+    {
+        $this->initialise();
+        $html = '<input type="text" name="SimplestSpam_challenge_field" class="text requiredField" />';
+        return $html;
+    }
 
-	public function Field($properties = array()) {
-		$this->initialise();
-		$html = '<input type="text" name="SimplestSpam_challenge_field" class="text requiredField" />';
-		return $html;
-	}
-
-	function FieldHolder() {
-		$this->initialise();
-		$obj = $this->getQuestionAnswerObject();
-		if(!$obj) {
-			return "";
-		}
-		$question = $obj->Question;
-		$siteConfig = SiteConfig::get()->First();
-		$explanation = $siteConfig->SimplestSpamExplanation;
-		if($explanation) {
-			$explanation = $explanation;
-		}
-		$Title = $this->XML_val('Title');
-		$Message = $this->XML_val('Message');
-		$MessageType = $this->XML_val('MessageType');
-		$Type = $this->XML_val('Type');
-		$extraClass = $this->XML_val('extraClass');
-		$Name = $this->XML_val('Name');
-		$Field = $this->XML_val('Field');
-		$messageBlock = (!empty($Message)) ? "<span class=\"message $MessageType\">$Message</span>" : "";
-		return <<<HTML
+    public function FieldHolder()
+    {
+        $this->initialise();
+        $obj = $this->getQuestionAnswerObject();
+        if (!$obj) {
+            return "";
+        }
+        $question = $obj->Question;
+        $siteConfig = SiteConfig::get()->First();
+        $explanation = $siteConfig->SimplestSpamExplanation;
+        if ($explanation) {
+            $explanation = $explanation;
+        }
+        $Title = $this->XML_val('Title');
+        $Message = $this->XML_val('Message');
+        $MessageType = $this->XML_val('MessageType');
+        $Type = $this->XML_val('Type');
+        $extraClass = $this->XML_val('extraClass');
+        $Name = $this->XML_val('Name');
+        $Field = $this->XML_val('Field');
+        $messageBlock = (!empty($Message)) ? "<span class=\"message $MessageType\">$Message</span>" : "";
+        return <<<HTML
 <div id="$Name" class="field $Type $extraClass">
 	<label class="left spamquestion">{$question} </label>
 	<div class="middleColumn">
@@ -65,67 +68,66 @@ class SimplestSpamField extends SpamProtectorField {
 	</div>
 </div>
 HTML;
-	}
+    }
 
-	public function validate($validator) {
-		$siteConfig = SiteConfig::get()->First();
-		// don't bother querying the SimplestSpam-service if fields were empty
-		if(
-			!isset($_REQUEST['SimplestSpam_challenge_field'])
-			|| empty($_REQUEST['SimplestSpam_challenge_field'])
-		) {
-			$validator->validationError(
-				$this->name,
-				$siteConfig->SimplestSpamWrongAnswerFieldMessage,
-				"validation",
-				false
-			);
-			Session::set("FormField.{$this->form->FormName()}.{$this->getName()}", $siteConfig->SimplestSpamWrongAnswerFieldMessage);
-			$this->form->sessionMessage($siteConfig->SimplestSpamWrongAnswerFormMessage, "bad");
-			return false;
-		}
-		$response = $_REQUEST['SimplestSpam_challenge_field'];
-		$obj = $this->getQuestionAnswerObject();
-		if(!$obj || !isset($obj->Answer)) {
-			user_error("SimplestSpamField::validate(): could not find answer - sorry, please try again'", E_USER_ERROR);
-			return false;
-		}
-		$answer = $obj->Answer;
-		if($this->cleanupAnswer($answer) != $this->cleanupAnswer($response)) {
-			$validator->validationError(
-				$this->name,
-				$siteConfig->SimplestSpamWrongAnswerFormMessage,
-				"validation",
-				false
-			);
-			Session::set("FormField.{$this->form->FormName()}.{$this->getName()}", $siteConfig->SimplestSpamWrongAnswerFieldMessage);
-			$this->form->sessionMessage($siteConfig->SimplestSpamWrongAnswerFormMessage, "bad");
-			return false;
-		}
-		return true;
-	}
+    public function validate($validator)
+    {
+        $siteConfig = SiteConfig::get()->First();
+        // don't bother querying the SimplestSpam-service if fields were empty
+        if (
+            !isset($_REQUEST['SimplestSpam_challenge_field'])
+            || empty($_REQUEST['SimplestSpam_challenge_field'])
+        ) {
+            $validator->validationError(
+                $this->name,
+                $siteConfig->SimplestSpamWrongAnswerFieldMessage,
+                "validation",
+                false
+            );
+            Session::set("FormField.{$this->form->FormName()}.{$this->getName()}", $siteConfig->SimplestSpamWrongAnswerFieldMessage);
+            $this->form->sessionMessage($siteConfig->SimplestSpamWrongAnswerFormMessage, "bad");
+            return false;
+        }
+        $response = $_REQUEST['SimplestSpam_challenge_field'];
+        $obj = $this->getQuestionAnswerObject();
+        if (!$obj || !isset($obj->Answer)) {
+            user_error("SimplestSpamField::validate(): could not find answer - sorry, please try again'", E_USER_ERROR);
+            return false;
+        }
+        $answer = $obj->Answer;
+        if ($this->cleanupAnswer($answer) != $this->cleanupAnswer($response)) {
+            $validator->validationError(
+                $this->name,
+                $siteConfig->SimplestSpamWrongAnswerFormMessage,
+                "validation",
+                false
+            );
+            Session::set("FormField.{$this->form->FormName()}.{$this->getName()}", $siteConfig->SimplestSpamWrongAnswerFieldMessage);
+            $this->form->sessionMessage($siteConfig->SimplestSpamWrongAnswerFormMessage, "bad");
+            return false;
+        }
+        return true;
+    }
 
-	protected function cleanupAnswer($v) {
-		return trim(strtolower($v));
-	}
+    protected function cleanupAnswer($v)
+    {
+        return trim(strtolower($v));
+    }
 
-	protected function getQuestionAnswerObject() {
-		$this->initialise();
-		$number = Session::get("SimplestSpamQuestion");
-		if($number > 0) {
-			$number = $number - 1;
-			if(($dos = SimplestSpamFieldQuestion::get()->limit(1, $number))->count()) {
-				return $dos->first();
-			}
-			else {
-				$this->error = _t("SimplestSpamField.QUESTIONNOTFOUND", "Selected question not found.");
-			}
-		}
-		else {
-			$this->error = _t("SimplestSpamField.QUESTIONSELECTIONNOTAVAILABLE", "No question selection made.");
-		}
-		return false;
-	}
-
-
+    protected function getQuestionAnswerObject()
+    {
+        $this->initialise();
+        $number = Session::get("SimplestSpamQuestion");
+        if ($number > 0) {
+            $number = $number - 1;
+            if (($dos = SimplestSpamFieldQuestion::get()->limit(1, $number))->count()) {
+                return $dos->first();
+            } else {
+                $this->error = _t("SimplestSpamField.QUESTIONNOTFOUND", "Selected question not found.");
+            }
+        } else {
+            $this->error = _t("SimplestSpamField.QUESTIONSELECTIONNOTAVAILABLE", "No question selection made.");
+        }
+        return false;
+    }
 }
